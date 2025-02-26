43ab47d8735e3d1d2f9c664448df27d7b9435929
OFRqq <ole.franssen@gmail.com>
2025-02-26 14:05:26 +0100

    #2 fix data fetch

--- server.js
+++ server.js
@@ -18,7 +18,23 @@ app.set('views', './views')
 app.use(express.urlencoded({extended: true}))
 
 
+app.get('/', async function (request, response) {
+  // Haal alle personen uit de WHOIS API op, van dit jaar
+  const personResponse = await fetch('https://fdnd.directus.app/items/person/?sort=name&fields=*,squads.squad_id.name,squads.squad_id.cohort&filter={"_and":[{"squads":{"squad_id":{"tribe":{"name":"FDND Jaar 1"}}}},{"squads":{"squad_id":{"name":"1G"}}},{"squads":{"squad_id":{"cohort":"2425"}}}]}');
+  // En haal daarvan de JSON op
+  const personResponseJSON = await personResponse.json();
 
+  const messagesResponse = await fetch(`https://fdnd.directus.app/items/messages/?filter={"for":"Team ${teamName}"}`);
+  const messagesResponseJSON = await messagesResponse.json();
+
+  console.log(personResponseJSON.data);
+
+  response.render('index.liquid', { 
+    teamName: teamName,
+    persons: personResponseJSON.data,
+    messages: messagesResponseJSON.data
+  })
+})
 
 app.post('/', async function (request, response) {
   await fetch('https://fdnd.directus.app/items/messages/', {
@@ -63,4 +79,3 @@ if (teamName == '') {
   console.log(`Application started on http://localhost:${app.get('port')}`)
   })
 }
-
